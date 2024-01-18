afdf1ac31ebf1ea0a8d1841a70bf8b2d7fc903d3
nikksharma07 <139550166+nikksharma07@users.noreply.github.com>
2024-01-18 22:21:54 +0530

    Update play.py
    Signed-off-by: nikksharma07 <139550166+nikksharma07@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -85,7 +85,7 @@ def stream_markup(_, chat_id):
     ]
     return buttons
 
-[InlineKeyboardButton(text=_["SUPPORT_CHANNEL"], callback_data="ᴜᴘᴅᴀᴛᴇs")],
+               , callback_data="ᴜᴘᴅᴀᴛᴇs")],
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
         [
